607127c55f480e5a7f945c1aa5829300aee3122d
sasqiaalia <auliasaskia93@gmail.com>
2022-06-10 19:04:06 +0700

    Studi Kasus 8

---
+++ output.h
@@ -0,0 +1,7 @@
+void matriks::output()
+{
+  cout<<"Jumlah yang lulus    = "<<total<<endl;
+  cout<<"Lulus terbesar       = " <<max<<"Pada baris ke-"<<jum_baris<<endl;
+  cout<<"Lulus terkecil       = " <<min<<"Pada baris ke-"<<jum_baris<<endl;
+  cout<<"Rata-rata yang lulus = "<<(double) total / 3<<endl;
+}
